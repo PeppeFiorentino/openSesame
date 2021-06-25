119e364546fcbb4bc1d609b0576f83e7a4673064
PeppeFiorentino <giuseppe@fiorentino.ovh>
2021-06-25 22:00:01 +0200

    Update openSesame.ts

--- openSesame.ts
+++ openSesame.ts
@@ -1,6 +1,6 @@
 import { parse } from "https://deno.land/std/flags/mod.ts";
 
-const DELAY = 3500;
+const DELAY = 4000;
 
 function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -8,15 +8,13 @@ function sleep(ms: number): Promise<void> {
 
 async function knock(host: string, port: number) {
   console.log("Knocking port: ", port);
-  host='http://'+host;
-  const url=new URL(host);
-  url.port=port.toString();
-  url.protocol="TCP";
-  const c=new AbortController();
-  //const id=setTimeout(()=>c.abort(),DELAY);
-  fetch(url,{signal:c.signal}).catch(()=>null)
+  host = "http://" + host;
+  const url = new URL(host);
+  url.port = port.toString();
+  url.protocol = "TCP";
+  const c = new AbortController();
+  fetch(url, { signal: c.signal }).catch(() => null);
   await sleep(DELAY);
-  //clearTimeout(id);
   c.abort();
 }
 
@@ -26,13 +24,6 @@ if (myargs.v || myargs.V || myargs.version) {
   Deno.exit();
 }
 
-
 const chain = myargs._.reduce((acc, port) => {
   return acc.then(() => knock(myargs.h, port as number));
 }, Promise.resolve());
-/*
-chain.then(() => sleep(4 * DELAY)).then(() => Deno.exit());
-
-
-myargs._.forEach(port=>{const k=knock(myargs.h, port as number);});
-*/
